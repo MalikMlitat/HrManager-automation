c1dcf4b5b48db295861957b3d6976048b1a8d3bf
Malik Mlitat <malik.mlitat@gmail.com>
2023-09-16 12:07:36 +0300

    init cypress project

---
+++ cypress/e2e/page-objects/LoginPage.ts
@@ -0,0 +1,41 @@
+class LoginPage {
+
+    elements=
+    {
+        userName: () => cy.getByPlaceHolder('Username'),
+        password: () => cy.getByPlaceHolder('Password'),
+        loginBtn: () => cy.get('button')
+    }
+
+    forgottPasswordElements=
+    {
+        // reset password
+        forgotPasswordInHomePageBtn: () => cy.get('.orangehrm-login-forgot-header'),
+        resetPasswordBtn: () => cy.get('.oxd-button--secondary'),
+        notifyResetPassword: () => cy.get('.orangehrm-card-container')
+
+    }
+
+    login(userName: string, password:string)
+    {
+        this.elements.userName().type(userName);
+        this.elements.password().type(password);
+
+        this.elements.loginBtn().click();
+    }
+
+    forgottPassword(userName: string)
+    {   
+        this.forgottPasswordElements.forgotPasswordInHomePageBtn().click();
+        cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode');
+
+        this.elements.userName().type(userName);
+        this.forgottPasswordElements.resetPasswordBtn().click();
+
+        cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/sendPasswordReset');
+        this.forgottPasswordElements.notifyResetPassword().contains('Reset Password link sent successfully');
+
+    }
+}
+
+export default LoginPage;
---
+++ cypress/e2e/specs/hr-loginpage.ts
@@ -0,0 +1,28 @@
+import LoginPage from "../page-objects/LoginPage"
+
+const loginObj:LoginPage = new LoginPage();
+
+describe('Login to the Home page', () => { 
+
+   
+    beforeEach(function()
+    {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    })
+
+    it('loginWithValidUserAndPassword',() => { 
+        loginObj.login('Admin', 'admin123')
+    });
+
+    it('ForgottPassword',() => { 
+        loginObj.forgottPassword('Admin')
+    });
+
+    it.only('Verify login', () => {
+        loginObj.login('Admin', 'admin123')
+        cy.request('https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/locations')
+        .then((response) => {
+            expect(response).property('status').to.equal(200)
+        }) // request
+    })
+})
