b87ecd8b2ece3c20cf530ed028136dc5257db1db
Malik Mlitat <malik.mlitat@gmail.com>
2023-10-15 11:21:04 +0300

    refactor

--- cypress/e2e/specs/checkShortlistedCandidate.spec.cy.ts
+++ cypress/e2e/specs/checkShortlistedCandidate.spec.cy.ts
@@ -9,8 +9,10 @@ import { CREATE_VACNCY_API } from "../../support/constants";
 
 import { CREATE_CANDIDATE_REQ } from "../../support/constants";
 
+import VacancyHelper from "../../support/helper/candidateHelper";
+
 const loginObj: LoginPage = new LoginPage();
-const addEmpl: addEmployee = new addEmployee();
+const vacancyhelper: VacancyHelper = new VacancyHelper();
 
 var createdEmpNumber: Number;
 var createdVacancyId: Number;
@@ -26,54 +28,23 @@ describe('Employee Functionality', () => {
 
     it('Check schedul interview for candidate', () => {
         // create user to assign to the created vacancy later
-        cy.api( CREATE_EMPL_REQ ).then((response) => {
-            expect(response).property('status').to.equal(200)
-            createdEmpNumber = response.body.data.empNumber;
+        vacancyhelper.createEmployeeViaAPI()
+            .then((response) => { createdEmpNumber = response.body.data.empNumber; })
             // create vacancy to be assigned to the new candidate
-            cy.api({
-                method: 'POST',
-                url: CREATE_VACNCY_API,
-                body: 
-                {
-                    name: faker.person.firstName() + " Job Name ",
-                    jobTitleId: 22, // JobcategoryId
-                    numOfPositions: null,
-                    description: "",
-                    status: true,
-                    isPublished: true,
-                    employeeId: createdEmpNumber
-                }
-            })
-        }).then((response) => {
-            expect(response.status).to.equal(200);
-            createdVacancyId = response.body.data.id;
+            .then(() => { vacancyhelper.create_vacancy(createdEmpNumber) })
             // API: create candidate <- save:UserID
-            cy.api(CREATE_CANDIDATE_REQ).then((response) => {
-                expect(response.status).to.equal(200);
-                candidateID = response.body.data.id;
-                // API (UserID): per URL change user to shortlisted candidate
-                cy.api({
-                    method: 'PUT',
-                    url: 'web/index.php/api/v2/recruitment/candidates/' + candidateID + '/shortlist',
-                    body:
-                    {
-                        note: null
-                    }
-                }).then((response) => {
-                    expect(response.status).to.equal(200);
-                    cy.log(response.body.data.action);
-
-                    expect(response.body.data.action.label).to.equal('Shortlisted');
-                })
+            .then((response) => {createdVacancyId = response.body.data.id})
+            .then(() => { vacancyhelper.createCandidateViaAPI() })
+                    .then((response) => { candidateID = response.body.data.id })
+                    // API (UserID): per URL change user to shortlisted candidate
+                    .then(() => { vacancyhelper.visitShortlistedCandidate(candidateID) })
+                    .then((response) => {
+                        expect(response.body.data.action.label).to.equal('Shortlisted');
+                    })
             })
-            // UI: with UserID visit candidate by url -> schedule interview
-            // add employee to add as interviewer
-            // UI: fill data (scheduler interview data: Interviewer, date...etc.)
-            // assertion interview scheduled
-
-        }
-        ) //then create user to assign to the created vacancy later
-
-    }) // end of create vacancy to be assigned to the new candidate
-
+        // UI: with UserID visit candidate by url -> schedule interview
+        // add employee to add as interviewer
+        // UI: fill data (scheduler interview data: Interviewer, date...etc.)
+        // assertion interview scheduled
+ 
 });
---
+++ cypress/support/helper/candidateHelper.ts
@@ -0,0 +1,68 @@
+
+import { faker } from '@faker-js/faker';
+import { CREATE_VACNCY_API } from "../../support/constants";
+import { CREATE_EMPL_API } from "../../support/constants";
+import { CREATE_EMPL_BODY } from "../../support/constants";
+
+import { CREATE_CANDIDATE_API } from "../../support/constants";
+import { CREATE_CANDIDATE_BODY } from "../../support/constants";
+
+
+class VacancyHelper {
+
+    create_vacancy(employeeId: Number) {
+        return cy.api({
+            method: 'POST',
+            url: CREATE_VACNCY_API,
+            body:
+            {
+                name: faker.person.firstName() + " Job Name ",
+                jobTitleId: 22, // JobcategoryId
+                numOfPositions: null,
+                description: "",
+                status: true,
+                isPublished: true,
+                employeeId: employeeId
+            }
+        }).then((response) => {
+            expect(response).property('status').to.equal(200)});
+
+    }
+
+
+    makeAPICALL(inputMethod: string, inputUrl:string, inputBody:any) {
+        return cy.api({
+            method: inputMethod,
+            url: inputUrl,
+            body: inputBody
+        }).then((response) => {
+            expect(response).property('status').to.equal(200)});
+
+    }
+
+    createEmployeeViaAPI()
+    {
+        return this.makeAPICALL('POST', CREATE_EMPL_API, CREATE_EMPL_BODY)
+
+    }
+
+    createCandidateViaAPI()
+    {
+        return this.makeAPICALL('POST', CREATE_CANDIDATE_API, CREATE_CANDIDATE_BODY)
+
+    }
+
+    visitShortlistedCandidate(inputCandidateID)
+    {
+        return this.makeAPICALL('PUT',
+        'web/index.php/api/v2/recruitment/candidates/' + inputCandidateID + '/shortlist',
+        {
+            note: null
+        }
+        
+        )
+
+    }
+}
+
+export default VacancyHelper;
