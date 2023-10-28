36733c40f219724487cc2c26390ee30e654c1585
Malik Mlitat <malik.mlitat@gmail.com>
2023-10-28 11:20:03 +0300

    Add upload TC

---
+++ cypress/e2e/specs/vacancy.spec.cy.ts
@@ -0,0 +1,43 @@
+
+import VacancyHelper from "../../support/helper/candidateHelper"
+import LoginPage from "../../support/page-objects/LoginPage"
+
+const loginObj: LoginPage = new LoginPage();
+const vacancyhelper: VacancyHelper = new VacancyHelper();
+
+describe('Vacancy Functionality', () => {
+
+    var createdEmpNumber: Number;
+    var createdVacancyId: Number;
+
+    beforeEach(function () {
+        loginObj.login('Admin', 'admin123');
+        vacancyhelper.createEmployeeViaAPI()
+            .then((response) => {
+                createdEmpNumber = response.body.data.empNumber;
+            })
+            // create vacancy to be assigned to the new candidate
+            .then(() => { vacancyhelper.create_vacancy(createdEmpNumber) })
+            .then((response) => { createdVacancyId = response.body.data.id })
+    })
+
+    it('Upload file to Vacancy - Scenario 2', () => {
+        /*
+        Given The system has a vacancy record
+        When The user opens the vacancy form on the edit mode for that vacancy
+        And The user clicks on Add button in the Attachments area
+        And The user uploads a file for that vacancy and saves the form
+        Then The file should be uploaded and added to vacancy
+        */
+        cy.visit("/web/index.php/recruitment/addJobVacancy/" + createdVacancyId)
+        cy.get('.oxd-button').contains('Add').click();
+        cy.wait(100);
+        cy.get('input[type="file"]').selectFile('cypress/fixtures/sample.pdf',{force: true})
+        // fix this, get the correct save button 
+        cy.get('.oxd-text').contains('Add Attachment').parent().get('.oxd-button').contains('Save').click();
+        //cy.get('.oxd-button').contains('type', 'submit').click({force: true})
+        //cy.get(':nth-child(3) > .oxd-form > .oxd-form-actions > .oxd-button--secondary').click({force: true})
+
+
+    });
+})
